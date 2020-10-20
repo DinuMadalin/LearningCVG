7844486850b8c99c09772e8dcb4bd34e5ea5cc9c
MBL-MADALIN\user <dinu.mihai94@gmail.com>
2020-10-20 19:42:54 +0300

    Am incercat sa fac sa se miste cubul din sageti , dar nu merge. Si am corectat sa inceama animatia de la primul click

--- Random/script.js
+++ Random/script.js
@@ -1,11 +1,26 @@
-(function(){
+(function click(){
   const cub = document.getElementById('cub');
-   var pos = Math.floor(Math.random() * 100) + 1;
+  var pos = Math.floor(Math.random() * 100) + 1;
   cub.addEventListener('click', () => {
     var pos = Math.floor(Math.random() * 100) + 1;
     var poss = Math.floor(Math.random() * 100) + 1;
       cub.style.bottom = pos + "%"; 
       cub.style.right =poss + "%";
-     cub.getElementsByClassName(linear);
+  })
+})()
+
+(function arrow() {
+  const cub = document.getElementById('cub');
+  cub.addEventListener('keydown', (event) => {
+    var a = Math.floor(Math.random() * 100) + 1;
+    var b = Math.floor(Math.random() * 100) + 1;
+    var c = Math.floor(Math.random() * 100) + 1;
+    var d = Math.floor(Math.random() * 100) + 1;
+    switch (event.key)
+    { case 'Arrowup' : parseInt(cub.style.bottom = a + "%");
+      case 'Arrowdown' :  parseInt(cub.style.bottom = b + "%");
+      case 'Arrowleft' :  parseInt(cub.style.right = c + "%");
+      case 'Arrowright' :  parseInt(cub.style.right = d + "%");
+    }
   })
 })()
